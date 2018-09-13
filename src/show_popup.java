a477618b2c903b9fe3ad9ae04367ac481eb3439d
Archcy <a892016758@gmail.com>
2018-09-13 21:36:44 +0800

    appearance change
    change some layout

--- src/show_popup.java
+++ src/show_popup.java
@@ -4,24 +4,26 @@ import java.awt.*;
 
 public class show_popup {
     private JWindow jw = new JWindow();
-    private int width=50,height=50,border=0; //button control
+    private int width=500,height=100,border=0; //button control
+    private int y_1=50;
     private int fontsize=50;
     private JPanel Qs_panel = new JPanel();
     private JLabel Caps_icon = new JLabel();
     private Font uf_Big = new Font("SansSerif",1,fontsize);
+    private float opca=0.3f;
     long time_dis=500;
     public void init_ind()
     {
-        jw.setSize(500,200);
-        jw.setOpacity(0.3f);
+        jw.setSize(width,height);
+        jw.setOpacity(opca);
         jw.setAlwaysOnTop(true);
         jw.setLocationRelativeTo(null);
         Color Button_bgc = Color.darkGray;
         Color fg= Color.white;
         Qs_panel.setBackground(Button_bgc);
         Qs_panel.setVisible(true);
-        Qs_panel.setBounds(border,border,width,height);
-        Caps_icon.setBounds(0,0,width,height);
+        Qs_panel.setBounds(border,border,width-border,height-border);
+        Caps_icon.setBounds(0,0,width-border,height-border);
         Caps_icon.setForeground(fg);
         Caps_icon.setFont(uf_Big);
         Qs_panel.add(Caps_icon);
@@ -33,9 +35,11 @@ public class show_popup {
             if(is_On)
             {
                 Caps_icon.setText("CapsLock ON");
+                jw.setOpacity(0.7f);
                 jw.setVisible(true);
                 Thread.currentThread().sleep(time_dis);
                 jw.dispose();
+                jw.setOpacity(opca);
             }
             else
             {
